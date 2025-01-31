d77f63bc88a4e83d593ecb1fb11434b3c95ae055
BrianTseng <xiaomingtseng@users.noreply.github.com>
2025-01-31 23:15:03 +0800

    deploy database

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -1,18 +1,21 @@
-from pymongo import MongoClient, ASCENDING
+import os
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
 
+load_dotenv()  # Load environment variables from .env file
 
 class Database:
-    def __init__(self, uri='cluster0-shard-00-00.xfe7j.mongodb.net:27017', db_name='mydatabase'):
-        self.client = MongoClient(uri)
-        self.db = self.client[db_name]
+    def __init__(self):
+        mongo_uri = os.getenv('MONGODB_URI')
+        if not mongo_uri:
+            raise ValueError("No MongoDB URI found in environment variables")
+        self.client = MongoClient(mongo_uri, server_api=ServerApi('1'))
+        self.db = self.client.get_database()
 
     def get_collection(self, collection_name):
         return self.db[collection_name]
 
-    def create_unique_index(self, collection_name, field_name):
-        collection = self.get_collection(collection_name)
-        collection.create_index([(field_name, ASCENDING)], unique=True)
-
-# 使用範例
-# db = Database()
-# collection = db.get_collection('mycollection')
+# Example usage:
+# db_instance = Database()
+# collection = db_instance.get_collection('your_collection_name')
